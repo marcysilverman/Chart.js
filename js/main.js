5d045d7f6a70a15b4b993915eb9c70bad29d9853
Marcy Silverman <marcysilve225@gmail.com>
2019-04-21 13:11:09 -0700

    added bar graph and radar graph

--- js/main.js
+++ js/main.js
@@ -1,13 +1,51 @@
 const chart = document.getElementById('lineChart');
-console.log(chart);
-
+const barChart = document.getElementById('barChart');
+const radarChart = document.getElementById('radarChart');
 let lineChart = new Chart(chart, {
-  type: 'bar',
+  type: 'line',
   data: {
       labels: ['Dog', 'Cat', 'Bird', 'Reptile', 'Fish', 'Hamster'],
       datasets: [{
           label: '# of Votes',
           data: [30, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 159, 64, 1)'
+          ]
+      }, 
+      {
+            label: 'Average Age of Animals',
+            data: [12, 14, 4, 12.3, 8, 3],
+            backgroundColor: [
+              'rgba(255, 159, 250, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }
+    ]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+    }
+  }
+});
+
+let barGraph = new Chart(barChart, {
+  type: 'bar',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
           backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
@@ -31,9 +69,36 @@ let lineChart = new Chart(chart, {
       scales: {
           yAxes: [{
               ticks: {
+                  reverse: true,
                   beginAtZero: true
               }
           }]
       }
+  }
+});
+
+let radarGraph = new Chart(radarChart, {
+  type: 'radar',
+  data: {
+      labels: ['Dog', 'Cat', 'Bird', 'Reptile'],
+      datasets: [{
+          label: '# of Votes',
+          data: [30, 19, 3, 5],
+          backgroundColor: [
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 159, 64, 1)'
+          ]
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+    }
   }
 });
